919dfcf76ae9d4961242996b8cdfd5750a508cb8
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-03 20:12:10 +0600

    model add done

---
+++ lib/models/chat_message_model.dart
@@ -0,0 +1,12 @@
+class ChatMessageModel {
+  final String role;
+  final List<ChatPartModel> parts;
+
+  ChatMessageModel({required this.role, required this.parts});
+}
+
+class ChatPartModel {
+  final String text;
+
+  ChatPartModel({required this.text});
+}
