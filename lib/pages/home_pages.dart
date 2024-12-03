d624d38d53a6dc5b6414ea04c0aa83317654d09f
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-03 19:34:07 +0600

    text field add done for search any text or image

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePages extends StatelessWidget {
@@ -15,8 +16,75 @@ class HomePages extends StatelessWidget {
                   "assets/space_bg.jpg",
                 ),
                 fit: BoxFit.cover)),
-        child: const Column(
-          children: [],
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: const Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Gemini",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Icon(
+                    Icons.image_search,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+            Expanded(child: ListView()),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 16),
+              // height: 120,
+              // color: Colors.blue,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      style: const TextStyle(color: Colors.black),
+                      cursorColor: Theme.of(context).primaryColor,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          fillColor: Colors.white,
+                          filled: true,
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor),
+                          )),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  CircleAvatar(
+                    radius: 33,
+                    backgroundColor: Colors.white,
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.grey[900],
+                      child: const Center(
+                          child: Image(
+                        image: AssetImage("assets/icon.png"),
+                        height: 35,
+                        width: 35,
+                        // color: Colors.white,
+                      )),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
